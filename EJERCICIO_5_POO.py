2bd7b13bdaca172c06fc75192aff8d28fb6e37a0
namontoya <161357317+namontoya@users.noreply.github.com>
2024-02-26 13:13:36 -0500

    Add files via upload

---
+++ EJERCICIO_5_POO.py
@@ -0,0 +1,14 @@
+#Ejercicio Propuesto No 17
+
+radio=9
+
+import math
+
+area=math.pi*radio**2
+longitud=2*math.pi*radio
+
+print("El radio es:", radio)
+print("El área es:", area)
+print("La longitud es:", longitud)
+
+
